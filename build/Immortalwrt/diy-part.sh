3face2794ea292af6b216adf312005f565b59edf
nnttm1122 <63239891+nnttm1122@users.noreply.github.com>
2024-09-15 16:53:15 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -104,6 +104,10 @@ openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz
 generic-squashfs-combined.vmdk
 generic-squashfs-combined-efi.vmdk
+generic-ext4-combined-efi.img.gz
+generic-ext4-rootfs.img.gz
+generic-rootfs.tar.gz
+generic-squashfs-combined.img.gz
 EOF
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
